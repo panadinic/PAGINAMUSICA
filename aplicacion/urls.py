23ec88f9e88cd8c4657c1ef4820ed5c4fa84da42
pablo nadinic <pa.nadinic@duocuc.cl>
2023-06-25 11:15:42 -0400

    comint fomingo

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -9,9 +9,10 @@ urlpatterns = [
     path('servicios/',ServiciosPageView.as_view(), name="servicios" ),
     path('clases/',ClasesPageView.as_view(), name="clases" ),
     path('iniciarSesion/',IniciarSesionPageView.as_view(), name="iniciarSesion" ),
-    path('crearCuenta/', CrearCuentaView.as_view(), name="crearCuenta"),
+    
     path('enviar_mensaje/',enviar_mensaje,name="enviar_mensaje"),
     ## path de spotify
+    path('crearCuenta/', CrearCuentaView.as_view(), name="crearCuenta"),
     path('crearCuenta/submit/', crear_cuenta, name="crearCuentaSubmit"),
     path('contacto/<int:total_carrito>/<str:servicios>/', ContactoPageView.as_view(), name="contacto_parametros"),
     
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -7,6 +7,8 @@ from django.views.generic import FormView
 from .models import Cliente
 from django.views.generic.base import TemplateView
 import pdb
+from django.views import View
+
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -25,80 +27,14 @@ class ClasesPageView(TemplateView):
 class IniciarSesionPageView(TemplateView):
     template_name = "aplicacion/iniciarSesion.html"
     
-# class CrearCuentaPageView(TemplateView):
-#     template_name = 'aplicacion/crearCuenta.html'
+
     
 class CrearCuentaView(FormView):
     template_name = 'aplicacion/crearCuenta.html'
     form_class = RegistroForm
-    # success_url = reverse_lazy('home')
 
 
-
-# def enviar_mensaje(request):
-#     if request.method == 'POST':
-#         nombre = request.POST['nombre']
-#         correo = request.POST['correo_electronico']
-#         servicio = request.POST['servicio']
-#         mensaje = request.POST['mensaje']
-
-      
-#         cliente = Cliente(nombre=nombre, correo_electronico=correo, servicio=servicio, mensaje=mensaje)
-#         cliente.save()
-
-#         print("Datos guardados en la base de datos.")
-
-#         # Puedes realizar alguna acción adicional después de guardar los datos, como mostrar un mensaje de éxito o redireccionar a otra página.
-#         return render(request, 'aplicacion/index.html')
-#     else:
-#         return render(request, 'aplicacion/iniciarSesion.html')
-
-
-
-# def enviar_mensaje(request):
-#     if request.method == 'POST':
-#         nombre = request.POST['nombre']
-#         correo = request.POST['correo_electronico']
-#         servicio = request.POST['servicio']
-#         mensaje = request.POST['mensaje']
-#         total_carrito = request.POST.get('total_carrito', '')
-#         servicios_seleccionados = request.POST.get('servicios', '')
-
-#         cliente = Cliente(nombre=nombre, correo_electronico=correo, servicio=servicio, mensaje=mensaje, total_carrito=total_carrito, servicios_seleccionados=servicios_seleccionados)
-#         cliente.save()
-
-#         print("Datos guardados en la base de datos.")
-
-#         # Puedes realizar alguna acción adicional después de guardar los datos, como mostrar un mensaje de éxito o redireccionar a otra página.
-#         return render(request, 'aplicacion/index.html')
-#     else:
-#         return render(request, 'aplicacion/iniciarSesion.html')
-
-
-
-
-# def enviar_mensaje(request):
-#     if request.method == 'POST':
-#         nombre = request.POST['nombre']
-#         correo = request.POST['correo_electronico']
-#         servicio = request.POST['servicio']
-#         mensaje = request.POST['mensaje']
-#         total_carrito = request.POST.get('total_carrito', '')  # Obtener el valor con una cadena vacía como valor predeterminado
-#         servicios_seleccionados = request.POST.get('servicios', '')  # Obtener el valor con una cadena vacía como valor predeterminado
-
-#         # Validar si total_carrito es una cadena vacía y asignar None en su lugar
-#         total_carrito = int(total_carrito) if total_carrito else None
-
-#         cliente = Cliente(nombre=nombre, correo_electronico=correo, servicio=servicio, mensaje=mensaje, total_carrito=total_carrito, servicios_seleccionados=servicios_seleccionados)
-#         cliente.save()
-
-#         print("Datos guardados en la base de datos.")
-
-#         # Puedes realizar alguna acción adicional después de guardar los datos, como mostrar un mensaje de éxito o redireccionar a otra página.
-#         return render(request, 'aplicacion/index.html')
-#     else:
-#         return render(request, 'aplicacion/iniciarSesion.html')
-
+  
 
 
 def enviar_mensaje(request):
@@ -130,11 +66,11 @@ def crear_cuenta(request):
         form = RegistroForm(request.POST)
         # pdb.set_trace() 
         if form.is_valid():
-            mensaje = "entramso al valid"
+            mensaje = "entramos al valid"
             print(mensaje)                                
             form.save()
             # Realiza acciones adicionales después de guardar los datos
-            return redirect('iniciarSesion')
+            return redirect('/iniciarSesion')
         else:
             mensaje = "esto no funciona"
             print(mensaje)
